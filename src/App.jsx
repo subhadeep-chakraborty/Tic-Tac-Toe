3fa38ff56f2ee26128745beb9b20bb23da05cc44
Subhadeep Chakraborty <85724371+subhadeep-chakraborty@users.noreply.github.com>
2023-11-08 22:25:00 +0530

    Player toggle on button click added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import Player from './components/Player.jsx'
 import GameBoard from './components/GameBoard.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [activePlayer, setActiveplayer]= useState('X');
+
+  function handleActivePlayer(){
+    setActiveplayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-        <Player initialName='Sam' symbol='X'/>
-        <Player initialName='Hank' symbol='O'/>
+        <ol id="players" className ="highlight-player">
+        <Player initialName='Sam' symbol='X' isActive={activePlayer === 'X'}/>
+        <Player initialName='Hank' symbol='O' isActive={activePlayer === 'O'}/>
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleActivePlayer} activePlayerSymbol={activePlayer}/>
       </div>
       LOG
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,16 +7,18 @@ const initialGameBoard =[
 ]
 
 
-export default function GameBoard() {
+export default function GameBoard({onSelectSquare, activePlayerSymbol}) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
             const updatedBoard =[...prevGameBoard.map(innerArray => [...innerArray])]
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         })
+        onSelectSquare();
     }
+   
     return(
         <ol id="game-board">
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
